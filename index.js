127428d9660fcbbc4e5deace5724b49ad944781e
adii1203 <aditya32ft@gmail.com>
2023-07-29 21:53:18 +0530

    port

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require("express");
 
 const app = express();
 
+const PORT = process.env.PORT;
 app.get("/", (req, res) => {
   res.send("<h1>hello world</h1>");
 });
 
-app.listen(3000, () => {
+app.listen(PORT || 8080, () => {
   console.log("app is listning");
 });
